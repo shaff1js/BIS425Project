ab68f26e14a0486b7e20288597dc76f280258a1f
shaff1js <shaff1js@CMU-LAB-2-084>
2018-02-01 17:16:52 -0500

    modified create parking lot form

--- 425Project/CreateParkingLotForm.Designer.vb
+++ 425Project/CreateParkingLotForm.Designer.vb
@@ -35,10 +35,6 @@ Partial Class CreateParkingLotForm
         Me.ParkingLotNameTextBox = New System.Windows.Forms.TextBox()
         Me.StreetAddressTextBox = New System.Windows.Forms.TextBox()
         Me.ParkingLotIDMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
-        Me.CapacityMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
-        Me.PriceMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
-        Me.BillStartTimeMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
-        Me.BillEndTimeMaskedTextBox = New System.Windows.Forms.MaskedTextBox()
         Me.CreateButton = New System.Windows.Forms.Button()
         Me.ClearButton = New System.Windows.Forms.Button()
         Me.BackButton = New System.Windows.Forms.Button()
@@ -49,6 +45,18 @@ Partial Class CreateParkingLotForm
         Me.Label12 = New System.Windows.Forms.Label()
         Me.CreateParkingLotToolTip = New System.Windows.Forms.ToolTip(Me.components)
         Me.Test1Label = New System.Windows.Forms.Label()
+        Me.CapacityTextBox = New System.Windows.Forms.TextBox()
+        Me.BeginBillHourComboBox = New System.Windows.Forms.ComboBox()
+        Me.BeginBillMinuteComboBox = New System.Windows.Forms.ComboBox()
+        Me.BeginBillAMPMComboBox = New System.Windows.Forms.ComboBox()
+        Me.EndBillHourComboBox = New System.Windows.Forms.ComboBox()
+        Me.EndBillMinuteComboBox = New System.Windows.Forms.ComboBox()
+        Me.EndBillAMPMComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.PriceDollarComboBox = New System.Windows.Forms.ComboBox()
+        Me.PriceCentComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label15 = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'Label1
@@ -154,50 +162,9 @@ Partial Class CreateParkingLotForm
         '
         Me.ParkingLotIDMaskedTextBox.Location = New System.Drawing.Point(139, 50)
         Me.ParkingLotIDMaskedTextBox.Name = "ParkingLotIDMaskedTextBox"
+        Me.ParkingLotIDMaskedTextBox.ReadOnly = True
         Me.ParkingLotIDMaskedTextBox.Size = New System.Drawing.Size(100, 20)
         Me.ParkingLotIDMaskedTextBox.TabIndex = 11
-        '
-        'CapacityMaskedTextBox
-        '
-        Me.CapacityMaskedTextBox.Location = New System.Drawing.Point(139, 128)
-        Me.CapacityMaskedTextBox.Mask = "000"
-        Me.CapacityMaskedTextBox.Name = "CapacityMaskedTextBox"
-        Me.CapacityMaskedTextBox.Size = New System.Drawing.Size(26, 20)
-        Me.CapacityMaskedTextBox.TabIndex = 12
-        Me.CapacityMaskedTextBox.Text = "000"
-        Me.CapacityMaskedTextBox.ValidatingType = GetType(Integer)
-        '
-        'PriceMaskedTextBox
-        '
-        Me.PriceMaskedTextBox.Location = New System.Drawing.Point(139, 154)
-        Me.PriceMaskedTextBox.Mask = "00.00"
-        Me.PriceMaskedTextBox.Name = "PriceMaskedTextBox"
-        Me.PriceMaskedTextBox.Size = New System.Drawing.Size(36, 20)
-        Me.PriceMaskedTextBox.TabIndex = 13
-        Me.PriceMaskedTextBox.Text = "0000"
-        Me.PriceMaskedTextBox.ValidatingType = GetType(Integer)
-        '
-        'BillStartTimeMaskedTextBox
-        '
-        Me.BillStartTimeMaskedTextBox.Location = New System.Drawing.Point(139, 180)
-        Me.BillStartTimeMaskedTextBox.Mask = "0000"
-        Me.BillStartTimeMaskedTextBox.Name = "BillStartTimeMaskedTextBox"
-        Me.BillStartTimeMaskedTextBox.Size = New System.Drawing.Size(36, 20)
-        Me.BillStartTimeMaskedTextBox.TabIndex = 14
-        Me.BillStartTimeMaskedTextBox.Text = "0000"
-        Me.CreateParkingLotToolTip.SetToolTip(Me.BillStartTimeMaskedTextBox, "Please use 24 hour format")
-        Me.BillStartTimeMaskedTextBox.ValidatingType = GetType(Date)
-        '
-        'BillEndTimeMaskedTextBox
-        '
-        Me.BillEndTimeMaskedTextBox.Location = New System.Drawing.Point(139, 206)
-        Me.BillEndTimeMaskedTextBox.Mask = "0000"
-        Me.BillEndTimeMaskedTextBox.Name = "BillEndTimeMaskedTextBox"
-        Me.BillEndTimeMaskedTextBox.Size = New System.Drawing.Size(36, 20)
-        Me.BillEndTimeMaskedTextBox.TabIndex = 14
-        Me.BillEndTimeMaskedTextBox.Text = "0000"
-        Me.CreateParkingLotToolTip.SetToolTip(Me.BillEndTimeMaskedTextBox, "Please use 24 hour format")
-        Me.BillEndTimeMaskedTextBox.ValidatingType = GetType(Date)
         '
         'CreateButton
         '
@@ -241,7 +208,7 @@ Partial Class CreateParkingLotForm
         '
         Me.Label9.AutoSize = True
         Me.Label9.ForeColor = System.Drawing.Color.Red
-        Me.Label9.Location = New System.Drawing.Point(171, 131)
+        Me.Label9.Location = New System.Drawing.Point(181, 131)
         Me.Label9.Name = "Label9"
         Me.Label9.Size = New System.Drawing.Size(11, 13)
         Me.Label9.TabIndex = 16
@@ -251,7 +218,7 @@ Partial Class CreateParkingLotForm
         '
         Me.Label10.AutoSize = True
         Me.Label10.ForeColor = System.Drawing.Color.Red
-        Me.Label10.Location = New System.Drawing.Point(181, 157)
+        Me.Label10.Location = New System.Drawing.Point(335, 157)
         Me.Label10.Name = "Label10"
         Me.Label10.Size = New System.Drawing.Size(11, 13)
         Me.Label10.TabIndex = 16
@@ -261,7 +228,7 @@ Partial Class CreateParkingLotForm
         '
         Me.Label11.AutoSize = True
         Me.Label11.ForeColor = System.Drawing.Color.Red
-        Me.Label11.Location = New System.Drawing.Point(181, 183)
+        Me.Label11.Location = New System.Drawing.Point(278, 183)
         Me.Label11.Name = "Label11"
         Me.Label11.Size = New System.Drawing.Size(11, 13)
         Me.Label11.TabIndex = 16
@@ -271,7 +238,7 @@ Partial Class CreateParkingLotForm
         '
         Me.Label12.AutoSize = True
         Me.Label12.ForeColor = System.Drawing.Color.Red
-        Me.Label12.Location = New System.Drawing.Point(181, 209)
+        Me.Label12.Location = New System.Drawing.Point(278, 209)
         Me.Label12.Name = "Label12"
         Me.Label12.Size = New System.Drawing.Size(11, 13)
         Me.Label12.TabIndex = 16
@@ -280,17 +247,131 @@ Partial Class CreateParkingLotForm
         'Test1Label
         '
         Me.Test1Label.AutoSize = True
-        Me.Test1Label.Location = New System.Drawing.Point(236, 186)
+        Me.Test1Label.Location = New System.Drawing.Point(349, 317)
         Me.Test1Label.Name = "Test1Label"
         Me.Test1Label.Size = New System.Drawing.Size(105, 13)
         Me.Test1Label.TabIndex = 17
         Me.Test1Label.Text = "Remove after testing"
+        '
+        'CapacityTextBox
+        '
+        Me.CapacityTextBox.Location = New System.Drawing.Point(139, 128)
+        Me.CapacityTextBox.MaxLength = 3
+        Me.CapacityTextBox.Name = "CapacityTextBox"
+        Me.CapacityTextBox.Size = New System.Drawing.Size(36, 20)
+        Me.CapacityTextBox.TabIndex = 18
+        '
+        'BeginBillHourComboBox
+        '
+        Me.BeginBillHourComboBox.FormattingEnabled = True
+        Me.BeginBillHourComboBox.Location = New System.Drawing.Point(139, 180)
+        Me.BeginBillHourComboBox.Name = "BeginBillHourComboBox"
+        Me.BeginBillHourComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.BeginBillHourComboBox.TabIndex = 19
+        '
+        'BeginBillMinuteComboBox
+        '
+        Me.BeginBillMinuteComboBox.FormattingEnabled = True
+        Me.BeginBillMinuteComboBox.Location = New System.Drawing.Point(194, 180)
+        Me.BeginBillMinuteComboBox.Name = "BeginBillMinuteComboBox"
+        Me.BeginBillMinuteComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.BeginBillMinuteComboBox.TabIndex = 19
+        '
+        'BeginBillAMPMComboBox
+        '
+        Me.BeginBillAMPMComboBox.FormattingEnabled = True
+        Me.BeginBillAMPMComboBox.Location = New System.Drawing.Point(236, 180)
+        Me.BeginBillAMPMComboBox.Name = "BeginBillAMPMComboBox"
+        Me.BeginBillAMPMComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.BeginBillAMPMComboBox.TabIndex = 19
+        '
+        'EndBillHourComboBox
+        '
+        Me.EndBillHourComboBox.FormattingEnabled = True
+        Me.EndBillHourComboBox.Location = New System.Drawing.Point(139, 206)
+        Me.EndBillHourComboBox.Name = "EndBillHourComboBox"
+        Me.EndBillHourComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.EndBillHourComboBox.TabIndex = 19
+        '
+        'EndBillMinuteComboBox
+        '
+        Me.EndBillMinuteComboBox.FormattingEnabled = True
+        Me.EndBillMinuteComboBox.Location = New System.Drawing.Point(194, 206)
+        Me.EndBillMinuteComboBox.Name = "EndBillMinuteComboBox"
+        Me.EndBillMinuteComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.EndBillMinuteComboBox.TabIndex = 19
+        '
+        'EndBillAMPMComboBox
+        '
+        Me.EndBillAMPMComboBox.FormattingEnabled = True
+        Me.EndBillAMPMComboBox.Location = New System.Drawing.Point(236, 206)
+        Me.EndBillAMPMComboBox.Name = "EndBillAMPMComboBox"
+        Me.EndBillAMPMComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.EndBillAMPMComboBox.TabIndex = 19
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label13.Location = New System.Drawing.Point(181, 183)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(11, 15)
+        Me.Label13.TabIndex = 20
+        Me.Label13.Text = ":"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label14.Location = New System.Drawing.Point(181, 207)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(11, 15)
+        Me.Label14.TabIndex = 20
+        Me.Label14.Text = ":"
+        '
+        'PriceDollarComboBox
+        '
+        Me.PriceDollarComboBox.FormattingEnabled = True
+        Me.PriceDollarComboBox.Location = New System.Drawing.Point(139, 153)
+        Me.PriceDollarComboBox.Name = "PriceDollarComboBox"
+        Me.PriceDollarComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.PriceDollarComboBox.TabIndex = 19
+        '
+        'PriceCentComboBox
+        '
+        Me.PriceCentComboBox.FormattingEnabled = True
+        Me.PriceCentComboBox.Location = New System.Drawing.Point(194, 153)
+        Me.PriceCentComboBox.Name = "PriceCentComboBox"
+        Me.PriceCentComboBox.Size = New System.Drawing.Size(36, 21)
+        Me.PriceCentComboBox.TabIndex = 19
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label15.Location = New System.Drawing.Point(181, 155)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(11, 15)
+        Me.Label15.TabIndex = 20
+        Me.Label15.Text = "."
         '
         'CreateParkingLotForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(484, 451)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.Label15)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.EndBillAMPMComboBox)
+        Me.Controls.Add(Me.BeginBillAMPMComboBox)
+        Me.Controls.Add(Me.EndBillMinuteComboBox)
+        Me.Controls.Add(Me.EndBillHourComboBox)
+        Me.Controls.Add(Me.BeginBillMinuteComboBox)
+        Me.Controls.Add(Me.PriceCentComboBox)
+        Me.Controls.Add(Me.PriceDollarComboBox)
+        Me.Controls.Add(Me.BeginBillHourComboBox)
+        Me.Controls.Add(Me.CapacityTextBox)
         Me.Controls.Add(Me.Test1Label)
         Me.Controls.Add(Me.Label12)
         Me.Controls.Add(Me.Label11)
@@ -300,10 +381,6 @@ Partial Class CreateParkingLotForm
         Me.Controls.Add(Me.BackButton)
         Me.Controls.Add(Me.ClearButton)
         Me.Controls.Add(Me.CreateButton)
-        Me.Controls.Add(Me.BillEndTimeMaskedTextBox)
-        Me.Controls.Add(Me.BillStartTimeMaskedTextBox)
-        Me.Controls.Add(Me.PriceMaskedTextBox)
-        Me.Controls.Add(Me.CapacityMaskedTextBox)
         Me.Controls.Add(Me.ParkingLotIDMaskedTextBox)
         Me.Controls.Add(Me.StreetAddressTextBox)
         Me.Controls.Add(Me.ParkingLotNameTextBox)
@@ -335,11 +412,7 @@ Partial Class CreateParkingLotForm
     Friend WithEvents ParkingLotNameTextBox As TextBox
     Friend WithEvents StreetAddressTextBox As TextBox
     Friend WithEvents ParkingLotIDMaskedTextBox As MaskedTextBox
-    Friend WithEvents CapacityMaskedTextBox As MaskedTextBox
-    Friend WithEvents PriceMaskedTextBox As MaskedTextBox
-    Friend WithEvents BillStartTimeMaskedTextBox As MaskedTextBox
     Friend WithEvents CreateParkingLotToolTip As ToolTip
-    Friend WithEvents BillEndTimeMaskedTextBox As MaskedTextBox
     Friend WithEvents CreateButton As Button
     Friend WithEvents ClearButton As Button
     Friend WithEvents BackButton As Button
@@ -349,4 +422,16 @@ Partial Class CreateParkingLotForm
     Friend WithEvents Label11 As Label
     Friend WithEvents Label12 As Label
     Friend WithEvents Test1Label As Label
+    Friend WithEvents CapacityTextBox As TextBox
+    Friend WithEvents BeginBillHourComboBox As ComboBox
+    Friend WithEvents BeginBillMinuteComboBox As ComboBox
+    Friend WithEvents BeginBillAMPMComboBox As ComboBox
+    Friend WithEvents EndBillHourComboBox As ComboBox
+    Friend WithEvents EndBillMinuteComboBox As ComboBox
+    Friend WithEvents EndBillAMPMComboBox As ComboBox
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents PriceDollarComboBox As ComboBox
+    Friend WithEvents PriceCentComboBox As ComboBox
+    Friend WithEvents Label15 As Label
 End Class
