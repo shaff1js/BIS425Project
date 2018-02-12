e9f17a9aa64b3c7ffd7401284b696ef5b9cb3c33
shaff1js <shaff1js@CMU-LAB-2-084>
2018-02-12 18:08:01 -0500

    Re did create statements for the variables.  ready to do initial test.  still need validation steps

--- 425Project/CreateParkingLotForm.vb
+++ 425Project/CreateParkingLotForm.vb
@@ -1,22 +1,62 @@
 ﻿Public Class CreateParkingLotForm
-    Dim ParkingLotNameString, StreetAddressString, BillStartTimeString, BillEndTimeString As String
+    Dim ParkingLotNameString, StreetAddressString, BillStartTimeString, BillEndTimeString, CapacityString, PriceString As String
     Dim CapacityInteger, BillingStartInteger, BillingEndInteger As Integer
-    Dim PriceDecimal As Decimal
+    Dim priceDecimal As Decimal
+
 
     Private Sub CreateButton_Click(sender As Object, e As EventArgs) Handles CreateButton.Click
         ParkingLotNameString = ParkingLotNameTextBox.Text
         StreetAddressString = StreetAddressTextBox.Text
-        CapacityInteger = Integer.Parse(CapacityTextBox.Text)
-        PriceDecimal = Decimal.Parse(PriceDollarComboBox.Text & PriceCentComboBox.Text)
-        ' BillingStartTime = Integer.Parse(BeginBillHourComboBox.Text & BeginBillMinuteComboBox.Text)
-        'BillingEndTime = Integer.Parse(EndBillHourComboBox.Text & EndBillMinuteComboBox.Text)
+
+        'create capacity string
+        Try
+            CapacityInteger = Integer.Parse(CapacityTextBox.Text)
+        Catch ex As Exception
+            MessageBox.Show("Capacity must be numeric and a whole number.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End Try
+
+        If CapacityInteger < 1 Then
+            MessageBox.Show("Capacity must be greater than 1.", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        Else
+            CapacityString = CapacityInteger.ToString
+        End If
+
+        'create price variable
+        PriceString = (PriceCentComboBox.Text & "." & PriceCentComboBox.Text)
+
+        If priceDecimal = Decimal.Parse(PriceString) <= 0 Then
+            MessageBox.Show("You must enter a price for parking", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
 
         'format billing start time
         If BeginBillAMPMComboBox.Text = "AM" Then
             BillStartTimeString = (BeginBillHourComboBox.Text & BeginBillMinuteComboBox.Text & "00")
         Else
+            BillingStartInteger = Integer.Parse(BeginBillHourComboBox.Text) + 12
+            If BillingStartInteger = 24 Then
+                BillStartTimeString = ("00" & BeginBillMinuteComboBox.Text & "00")
+            Else
+                BillStartTimeString = (BillingStartInteger.ToString & BeginBillMinuteComboBox.Text & 0)
+            End If
+        End If
 
+        'formet bill end time
+        If EndBillAMPMComboBox.Text = "AM" Then
+            BillEndTimeString = (EndBillHourComboBox.Text & EndBillMinuteComboBox.Text & "00")
+        Else
+            BillingEndInteger = Integer.Parse(EndBillHourComboBox.Text) + 12
+            If BillingEndInteger = 24 Then
+                BillEndTimeString = ("00" & EndBillMinuteComboBox.Text & "00")
+            Else
+                BillEndTimeString = (BillingEndInteger.ToString & EndBillMinuteComboBox.Text & "00")
+            End If
+        End If
 
+        If BillingStartInteger = (Integer.Parse(BillStartTimeString)) < BillingEndInteger = Integer.Parse(BillEndTimeString) Then
+            MessageBox.Show("Billing Start Time must be before Billing End Time", "Data Entry Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End If
         Test1Label.Text = BillStartTimeString
     End Sub
