d89a3b2e5510a991e059bb9d28b7af516ad2cf92
wolfe1kn <stwolfe1kn@CMU-LAB-045>
2018-02-12 19:00:50 -0500

    please see button

--- 425Project/StudentPortalForm.Designer.vb
+++ 425Project/StudentPortalForm.Designer.vb
@@ -22,16 +22,29 @@ Partial Class StudentPortalForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(182, 194)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "FUck Off"
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'StudentPortalForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(452, 451)
+        Me.Controls.Add(Me.Button1)
         Me.Name = "StudentPortalForm"
         Me.Text = "StudentPortalForm"
         Me.ResumeLayout(False)
 
     End Sub
+
+    Friend WithEvents Button1 As Button
 End Class
